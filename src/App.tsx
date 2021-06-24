079d712a99c49e648b4b5b920c1ba5990a1a87d8
unknown <espjoemusic@gmail.com>
2021-06-24 14:33:49 -0700

    Game working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styled from 'styled-components';
+import { Value } from './GameState';
+import { BoardState, useGameState } from './GameState';
+
+type LayoutProps = {
+  gap: number,
+}
+
+const Row = styled.div<LayoutProps>`
+    display: flex;
+    flex-direction: row;
+    gap: ${(props) => props.gap}px;
+  `;
+
+const Col = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.gap}px;
+`;
 
 function App() {
+  const {
+    gameState,
+    current, 
+    xIsNext,
+    winner,
+    handleClick,
+    jumpTo
+  } = useGameState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Row gap={20}>
+     <Col gap={20}>
+        <div> {
+          winner ? `Winner' ${winner}`
+          : `Next Player ${xIsNext ? 'X' : 'O'}`
+        }</div>
+      <Board board={current} onClick={handleClick} />
+    </Col>
+    <Log history={gameState.history} jumpTo= {jumpTo}/>
+  </Row>
+   
+  );
+}
+
+type BoardProps = {
+  board: BoardState,
+  onClick: (square: number) => void,
+}
+
+function Board({board, onClick}: BoardProps) {
+  const createProps = (square: number) : SquareProps => {
+    return {
+      value: board[square],
+      onClick: () => onClick(square),
+    }
+  };
+
+  return (
+    <Col gap={0}>
+      <Row gap={0}>
+        <Square {...createProps(0)}/>
+        <Square {...createProps(1)}/>
+        <Square {...createProps(2)}/>
+      </Row>
+      <Row gap={0}>
+        <Square {...createProps(3)}/>
+        <Square {...createProps(4)}/>
+        <Square {...createProps(5)}/>
+      </Row>
+      <Row gap={0}>
+        <Square {...createProps(6)}/>
+        <Square {...createProps(7)}/>
+        <Square {...createProps(8)}/>
+      </Row>
+    </Col>
+  );
+}
+
+const StyledSquare = styled.button`
+  width: 34px;
+  height: 34px;
+  background: #fff;
+  border: 1px solid #999;
+  padding: 0;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+type SquareProps = {
+  value: Value,
+  onClick: () => void,
+}
+
+function Square(props: SquareProps) {
+  return (
+    <StyledSquare onClick={props.onClick}>
+      {props.value}
+    </StyledSquare>
+  );
+}
+
+type LogProps = {
+  history: BoardState[],
+  jumpTo: (step: number) => void,
+}
+
+function Log(props: LogProps) {
+  return (
+    <ol>
+      {props.history.map((_, index) => {
+        return (
+          <li key={index}>
+            <button onClick={() => props.jumpTo(index)}>
+              Go to {index === 0 ? 'start' : `move #${index}`}
+            </button>
+          </li>
+        );
+      })}
+    </ol>
   );
 }
 
